4effca01b06b9158f3423090020f854cd8678ab1
alexk49 <alexcode9@mailbox.org>
2024-07-02 21:06:04 +0100

    added lower limit on container size

--- script.js
+++ script.js
@@ -113,8 +113,8 @@ submitGridSizeButton.addEventListener('click', () => {
 const submitContainerSizeButton = document.querySelector('#submit-container-size')
 submitContainerSizeButton.addEventListener('click', () => {
   let containerSize = document.getElementById('container-size').value
-  while (containerSize < 2 || containerSize > 1000) {
-    containerSize = Number(prompt("That's not going to work. Enter a number between 2 and 1000", 600))
+  while (containerSize < 340 || containerSize > 1000) {
+    containerSize = Number(prompt("That's not going to work. Enter a number between 340 and 1000", 600))
   }
   const grid = document.querySelector('#grid')
   grid.style.width = containerSize + 'px'
